0e5ff3b1a5067bb4a19c4a84aefb23c4c98b4b10
rajatsachdeva <rajat.sachdeva621@gmail.com>
2017-01-09 21:37:20 +0530

    added hello world

---
+++ KandR/ch1/Exercises/1.1/hello_world.c
@@ -0,0 +1,15 @@
+/*************************************************
+**
+**          HELLO WORLD PROGRAM
+**
+**
+*************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello, World!\n");
+    return EXIT_SUCCESS;
+}
