20c5155691ef2d1e967cc6596d42bec6b636864f
Rajat Sachdeva <rajat.sachdeva621@gmail.com>
2017-07-08 14:33:32 +0530

    How not to use string copy functions
    How not to use string copy functions ?

---
+++ string_copy/strin_copy.c
@@ -0,0 +1,12 @@
+/* 
+ This file depcits the use of 
+ strcpy, strcpy_s, strncpy, strncpy_s
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+#define HELLO "Hello"
+#define 
+
+/* way not to use string copy functions */
