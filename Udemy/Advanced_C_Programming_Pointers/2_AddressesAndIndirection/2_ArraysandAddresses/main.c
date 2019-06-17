218910c1d3a57a1b46a53ca72a8048d7d4d20606
Rajat Sachdeva <rajat.sachdeva621@gmail.com>
2019-06-18 01:17:29 +0530

    updated arrays and addresses

--- Udemy/Advanced_C_Programming_Pointers/2_AddressesAndIndirection/2_ArraysandAddresses/main.c
+++ Udemy/Advanced_C_Programming_Pointers/2_AddressesAndIndirection/2_ArraysandAddresses/main.c
@@ -3,6 +3,8 @@
 int main(int argc, char **argv) {
     char str1[] = "Hello";
     char *str2 = "Goodbye";
+    //str2 = str1;
+    str2 = &str1;
     printf("%p %p %s\n", &str1, str1, str1);
     printf("%p %p %p %s\n", &str2, str2, &str2[0], str2);
     return 0;
