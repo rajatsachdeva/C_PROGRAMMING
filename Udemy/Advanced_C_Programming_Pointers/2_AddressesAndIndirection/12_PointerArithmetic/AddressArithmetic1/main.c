5741a40fb4a97f97906171b0734a2759ea4cfeaf
Rajat Sachdeva <rajat.sachdeva621@gmail.com>
2019-06-26 00:30:27 +0530

    Updated pointerArithmetic

--- Udemy/Advanced_C_Programming_Pointers/2_AddressesAndIndirection/12_PointerArithmetic/AddressArithmetic1/main.c
+++ Udemy/Advanced_C_Programming_Pointers/2_AddressesAndIndirection/12_PointerArithmetic/AddressArithmetic1/main.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 
 #define LENGTH 3
+#define COUNT 4
 
 int data[LENGTH] = {0};            // some integers
 
+// basic address arithmetic
+
 int main(int argc, char **agrv) {
-    int *pi         = NULL; // a simple pointer to an integer
-    int **ppi       = NULL; // a pointer to a pointer to an integer
     int i           = 0;
+    int *p          = NULL;
+    int a[COUNT]    = {0};
 
     printf("Multiple Indirection example\n");
 
@@ -27,6 +30,42 @@ int main(int argc, char **agrv) {
     for (i = 0; i < LENGTH; i++) {
         printf("data[%d] = %d\n", i, data[i]);
     }
+
+    printf("\n");
+    // first let's look at the size of a basic integer
+    printf("size of an 'int' is %lu\n", sizeof(int));
+
+    // initialize array
+    for(i = 0; i < COUNT; i++) {
+        a[i] = i;
+    }
+    // print the array
+    for (i = 0; i < COUNT; i++) {
+        printf("a[%d] = %d\n", i, a[i]);
+    }
+    printf("\n");
+
+    // let's do some pointer arithmetic
+    p = a;
+    // set the pointer to the array to the start of the array
+    printf("address of a is %p; value of p is %p [%u]; value pointed to by p is %d\n", a, p, p, *p);
+
+    // increment p by 1
+    // NOTE: the address of 'a' remains the same (because we haven't done anything to it)
+    // but the value of 'p' has been incremented by 4 - the size of an integer - and so ...
+    // the value pointed to by 'p' is now a[1]
+    p = p + 1;
+    printf("address of a is %p; value of p is %p [%u]; value pointed to by p is %d\n", a, p, p, *p);
+
+    // increment p by another 2 the value pointed to by 'p' is now a[3]
+	p = p + 2;
+    printf("address of a is %p; value of p is %p [%u]; value pointed to by p is %d\n", a, p, p, *p);
+
+    // what happens if we go beyond the array end? there's nothing to stop you or warn you ...
+	// ... so, increment p by another 1 the value pointed to by 'p' is now a[4]
+	// ... but we will get junk
+	p = p + 1;
+	printf("address of a is %p; value of p is %p [%u]; value pointed to by p is %d\n", a, p, p, *p);
 
     return 0;
 }
